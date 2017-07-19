0a4f076b2c6a66a193c0c4339b3da579435712f2
DESKTOP-KOQMP2O\Ekapob <ekapob.art@hotmail.com>
2017-07-19 18:07:08 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,4 +1,33 @@
 <?php
+function callService($url,$cache=0){
+	
+
+    $cache_file = 'cache/'.md5($url);
+
+    if (file_exists($cache_file) && $cache>0 && (filemtime($cache_file) > (time() - 60 * $cache ))) {
+	 
+	   $output = file_get_contents($cache_file);
+	} else {
+	  
+	   $ch = curl_init();
+	   curl_setopt($ch, CURLOPT_URL, $url);
+	   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  
+	   $output = curl_exec($ch);
+	   curl_close($ch);  
+	   file_put_contents($cache_file, $output, LOCK_EX);
+	}
+
+
+    return  json_decode($output);    
+}
+
+$wallet = str_replace('0x', '', $_GET['miner']);
+$bx_price_url = "https://bx.in.th/api/";
+$bx_price = callService($bx_price_url,1);
+
+
+
 $access_token = 'KqbT+kBzOYBY7UJoOqa/ZXcaldF2/wwBkY++L4XdDatpJj+FcT2X5z8GJGEDhKij9I5taNONaFM9TvG+MLAvcyf1/TB+tkkFRhiEtUKw2nxf1t2D/pU8VWRvBppfOvt213geEIRcmqqOc4MSIVbT2QdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -21,6 +50,13 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+
+			if($text=="จาวิส ราคา ETH"){
+				$messages = [
+					'type' => 'text',
+					'text' => 'ETH ราคา '.$bx_price->{21}->last_price.' บาทเด้อ'
+				];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
