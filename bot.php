e346ab972dcd41009700065d8ff01e2cffe52413
DESKTOP-JL35TL8\IceDogZ <ekapob.art@hotmail.com>
2017-07-20 00:30:58 +0700

    no message

--- bot.php
+++ bot.php
@@ -199,9 +199,12 @@ ZEC : '.number_format($zec[0]->price_thb,2).' บาท'
 			}
 
 			if (preg_match('/จาวิส/',$text) && $match_count==0){
+				$msg = array('จักนำเด้อคับลูกพี่','จักเว่าพิสังดอก','ช่วยพูดให้เข้าใจหน่อย','โอ้ย ถามพิสังน้อ');
+				$k = array_rand($msg);
+				$v = $msg[$k];
 				$messages = [
 					'type' => 'text',
-					'text' => 'จักนำเด้อคับลูกพี่ จักเว่าพิสังดอก'
+					'text' => $v
 				];	
 				$match_count = $match_count+1;
 			}
