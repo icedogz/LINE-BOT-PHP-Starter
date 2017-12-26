51d191de50f0fe71ea11033b34dd4a49888017e2
DESKTOP-KOQMP2O\Ekapob <ekapob.art@hotmail.com>
2017-12-26 16:54:34 +0700

    no message

--- job.php
+++ job.php
@@ -8,11 +8,7 @@ $zec =callService('https://api.coinmarketcap.com/v1/ticker/zcash/?convert=THB',1
 
 $messages = [
 			'type' => 'text',
-			'text' => 'อัพเดทราคา
-ETH : '.number_format($bx_price->{21}->last_price,2).' บาท
-BTC : '.number_format($bx_price->{1}->last_price,2).' บาท
-ETC : '.number_format($etc[0]->price_thb,2).' บาท
-ZEC : '.number_format($zec[0]->price_thb,2).' บาท'
+			'text' => 'โย่ๆๆ โย่ๆๆ'
 		];	
 
 pushMessage($messages);		
