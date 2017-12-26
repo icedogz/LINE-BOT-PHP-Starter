46473ea0dbe7e31e7f31289bafc1c5506939bbc7
DESKTOP-KOQMP2O\Ekapob <ekapob.art@hotmail.com>
2017-12-26 17:02:21 +0700

    no message

--- job.php
+++ job.php
@@ -1,11 +1,6 @@
 <?php
 include "lib.php";
 
-$bx_price_url = "https://bx.in.th/api/";
-$bx_price = callService($bx_price_url,1);
-$etc =callService('https://api.coinmarketcap.com/v1/ticker/ethereum-classic/?convert=THB',1);
-$zec =callService('https://api.coinmarketcap.com/v1/ticker/zcash/?convert=THB',1);
-
 $messages = [
 			'type' => 'text',
 			'text' => $_GET['message'] 
